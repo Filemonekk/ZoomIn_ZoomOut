e21fa146bd26a7ad1d5ff0b16d55a933459dbc5f
KasiaH <kah.gda@wp.pl>
2024-07-19 13:07:31 +0200

    added font button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sizeUp = document.querySelector('.sizeUp')
 const sizeDown = document.querySelector('.sizeDown')
 const colorBtn = document.querySelector('.color')
+const fontBtn = document.querySelector('.font')
 const p = document.querySelector('p')
 
 let fontSize = 36
@@ -24,10 +25,29 @@ const colorChanger = () => {
     const g = Math.floor(Math.random() * 255)
     const b = Math.floor(Math.random() * 255)
     
-p.style.color = `rgb(${r},${g},${b})`
+    p.style.color = `rgb(${r},${g},${b})`
+};
+
+const fonts = [
+    "baloo-bhaina",
+    "josefin-slab",
+    "arvo",
+    "lato",
+    "volkhov",
+    "abril-fatface",
+    "ubuntu",
+    "roboto",
+    "droid-sans-mono",
+    "anton",
+];
+
+const fontCharger = (fonts) => {
+    const number = fonts[Math.floor(Math.random() * fonts.length)];
+    p.style.fontFamily = fonts[number]
 }
 
 
 sizeUp.addEventListener('click', increase)
 sizeDown.addEventListener('click', decrease)
-colorBtn.addEventListener('click', colorChanger )
+colorBtn.addEventListener('click', colorChanger)
+fontBtn.addEventListener('click', fontCharger)
