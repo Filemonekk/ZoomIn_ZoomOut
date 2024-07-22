1ccbfe1ce262b5976cbd8fe76cb432abfb27b0da
KasiaH <kah.gda@wp.pl>
2024-07-22 20:09:21 +0200

    code review

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ let fontSize = 36
 
 const increase = () => {
     if(fontSize>200) return
-    fontSize += 5    
-    p.style.fontSize = fontSize + 'px'   
+    fontSize += 5
+    p.style.fontSize = fontSize + 'px'
 }
-
-
 
 const decrease = () => {
     if(fontSize<=21) return
@@ -24,7 +22,7 @@ const colorChanger = () => {
     const r = Math.floor(Math.random() * 255)
     const g = Math.floor(Math.random() * 255)
     const b = Math.floor(Math.random() * 255)
-    
+
     p.style.color = `rgb(${r},${g},${b})`
 };
 
@@ -38,16 +36,13 @@ const fonts = [
     "Droid Sans Mono",
     "Anton"
 ];
-  
+
 const fontCharger = () => {
     let number = Math.floor(Math.random() * fonts.length);
     p.style.fontFamily = fonts[number]
 }
 
-
 sizeUp.addEventListener('click', increase)
 sizeDown.addEventListener('click', decrease)
 colorBtn.addEventListener('click', colorChanger)
 fontBtn.addEventListener('click', fontCharger)
-
-//the font button doesn't work//
